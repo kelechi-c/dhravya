cafc98a60a2325d36bf9874831947e98a27d968a
Tensor <narutokelechi@gmail.com>
2024-06-14 11:54:34 +0100

    Add text formattin, clean up text file

--- data_scripts/pdf_text_extract.py
+++ data_scripts/pdf_text_extract.py
@@ -1,23 +1,29 @@
-import rich, os
+import rich, os, re
 import argparse
 from PyPDF2 import PdfReader
 from tqdm.auto import tqdm
 from rich import traceback
 
-traceback.install(show_locals=True)
+traceback.install(show_locals=True) # for beautiful traceback messages
 
-
+# cli arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("path")
 parser.add_argument("textfile")
 args = parser.parse_args()
 
-base_folder = "ignore/pdf_data"
+
+# get pdf file paths
+base_folder = "pdf_data"
 pdf_list = os.listdir(args.path)
 pdf_list = [os.path.join(base_folder, pdfile) for pdfile in pdf_list]
 
-text_file = 'dhravya_data.txt'
 
+def clean_text(text):
+    regex_format = r"[^\w\s\d!@#\$%\&'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\[\]\^\_\{\|\}~]"  # Leaves behind onl
+    cleaned_text = re.sub(regex_format, '', text) 
+
+    return cleaned_text
 
 def pdf2text_file(pdf_file: str):
     extract_text = ""
@@ -26,8 +32,9 @@ def pdf2text_file(pdf_file: str):
     for page_num in tqdm(range(len(pdf_read.pages))):
         page = pdf_read.pages[page_num]
         extract_text += page.extract_text()
+        extract_text = clean_text(extract_text)
 
-    with open(text_file, 'w', encoding='utf-8') as file:
+    with open('sample.txt', 'w', encoding='utf-8') as file:
         file.write(extract_text)
     
     return extract_text
@@ -37,18 +44,23 @@ def merge_pdfs(file_list: list, out_text_file: str):
     text_corpus = ''
 
     try:
-        for pdf in tqdm(file_list, colour='green'):
+        for pdf in tqdm(file_list, colour='cyan'): # extract from pdf in queue
             text_extract = pdf2text_file(pdf)
-            text_corpus += text_extract
+            text_corpus += text_extract # concat text and add space before the next one is added
+            text_corpus += '\n  '
+            rich.print(f'Extracted [white] {pdf}')
+            
 
         with open(out_text_file, 'w', encoding='utf-8') as file:
-            file.write(text_corpus)
-
-        rich.print(f"PDFs extracted to single text file [bold green]{out_text_file}[/bold green]")
+            file.write(text_corpus) # write to text file
+            
+        # success message
+        rich.print(f"{len(pdf_list)} research paper PDFs extracted to single text file [bold green]{out_text_file}[/bold green]")
         return text_corpus
 
+    # exception hanndling and colored output
     except Exception as e:
-        rich.print(f'[bold red] Error in extraction --> {e}')
+        rich.print(f'[bold red] Error in extraction --> {e}') 
 
-# if __name__=='main':
+
 merge_pdfs(pdf_list, args.textfile)
